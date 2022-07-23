094c4193a1fde049a1930a2df60186c0d3185c67
Matt54 <mattpfeiffer54@gmail.com>
2022-07-23 11:48:10 -0500

    Added color hex initializer
    Added color hex initializer

--- Sources/UtilityAndBeyond/Extensions/Color+Extensions.swift
+++ Sources/UtilityAndBeyond/Extensions/Color+Extensions.swift
@@ -36,6 +36,29 @@ public extension Color {
     }
 }
 
+public extension Color {
+    init(hex: String) {
+        var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+
+        if (cString.hasPrefix("#")) {
+            cString.remove(at: cString.startIndex)
+        }
+
+        if ((cString.count) != 6) {
+            self.init(white: 0.5)
+        }
+
+        var rgbValue:UInt64 = 0
+        Scanner(string: cString).scanHexInt64(&rgbValue)
+
+        self.init(.displayP3,
+                          red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+                          green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+                          blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+                          opacity: CGFloat(1.0))
+    }
+}
+
 #if os(OSX)
 
 extension Color {
