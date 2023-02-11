a526859e479bad2838e1aa1b5696780cc235d3ba
Matt54 <mattpfeiffer54@gmail.com>
2023-02-11 09:54:31 -0600

    Made centering the window optional

--- Sources/UtilityAndBeyond/Extensions/View+Extensions.swift
+++ Sources/UtilityAndBeyond/Extensions/View+Extensions.swift
@@ -3,14 +3,19 @@ import SwiftUI
 // MARK: View+NewWindow
 #if os(OSX)
 public extension View {
-    private func newWindowInternal(title: String, geometry: NSRect, style: NSWindow.StyleMask, delegate: NSWindowDelegate, windowEscape: ((NSWindow) -> Void)? = nil) -> NSWindow {
+    private func newWindowInternal(title: String,
+                                   geometry: NSRect,
+                                   shouldCenter: Bool = true,
+                                   style: NSWindow.StyleMask,
+                                   delegate: NSWindowDelegate,
+                                   windowEscape: ((NSWindow) -> Void)? = nil) -> NSWindow {
         let window = NSWindow(
             contentRect: geometry,
             styleMask: style,
             backing: .buffered,
             defer: false
         )
-        window.center()
+        if shouldCenter { window.center() }
         window.isReleasedWhenClosed = false
         window.title = title
         window.makeKeyAndOrderFront(nil)
@@ -24,8 +29,13 @@ public extension View {
         return window
     }
    
-    func openNewWindow(title: String, delegate: NSWindowDelegate, geometry: NSRect = NSRect(x: 20, y: 20, width: 640, height: 480), style: NSWindow.StyleMask = [.titled, .closable, .miniaturizable, .resizable], windowEscape: ((NSWindow) -> Void)? = nil) {
-        newWindowInternal(title: title, geometry: geometry, style: style, delegate: delegate, windowEscape: windowEscape).contentView = NSHostingView(rootView: self)
+    func openNewWindow(title: String,
+                       delegate: NSWindowDelegate,
+                       geometry: NSRect = NSRect(x: 20, y: 20, width: 640, height: 480),
+                       shouldCenter: Bool = true,
+                       style: NSWindow.StyleMask = [.titled, .closable, .miniaturizable, .resizable],
+                       windowEscape: ((NSWindow) -> Void)? = nil) {
+        newWindowInternal(title: title, geometry: geometry, shouldCenter: shouldCenter, style: style, delegate: delegate, windowEscape: windowEscape).contentView = NSHostingView(rootView: self)
     }
 }
 #endif
